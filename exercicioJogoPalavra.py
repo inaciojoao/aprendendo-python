70ab6fe6fbd234bedc1728b9de6ec7e375935935
João Gabriel <joaogabrielinacio60@gmail.com>
2023-01-18 21:35:06 -0300

    segundo projeto em python

---
+++ exercicioJogoPalavra.py
@@ -0,0 +1,33 @@
+import os
+
+palavra_secreta = 'vodka'
+letras_acertadas = ""
+numero_tentativa = 0
+while True:
+    entrada_usuario = input('Digite uma letra: ')
+    numero_tentativa +=1
+
+    if len(entrada_usuario) > 1:    #esse blobo serve para checar se o usuario digitou apenas uma letra
+        print('Digite apenas uma letra. ')
+        continue
+
+    print('Bora lá continuar essa bagaça.')
+
+    if entrada_usuario in palavra_secreta:
+        letras_acertadas += entrada_usuario
+
+    palavra_formada = ''
+    for letra_secreta in palavra_secreta:
+        if letra_secreta in letras_acertadas:
+            palavra_formada += letra_secreta
+        else:
+            palavra_formada += '*'
+
+    print('Palavra formada: ', palavra_formada)
+
+    if palavra_formada == palavra_secreta:
+        os.system('cls')
+        print('PARABÉNS VC ACERTOU A PALAVRA SECRETA!!')
+        print('Número total de tentativas: ', numero_tentativa)
+
+
